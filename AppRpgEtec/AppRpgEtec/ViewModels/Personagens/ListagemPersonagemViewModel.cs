b5b1494c98d06707d555a1d94730336633d31b92
unknown <ritafilhotes4@gmail.com>
2025-05-26 09:00:20 -0300

    :sparkles: Feat: Modelar o projeto com as novas Classes e CRUD.

---
+++ AppRpgEtec/AppRpgEtec/ViewModels/Personagens/ListagemPersonagemViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppRpgEtec.Models;
+using AppRpgEtec.Services.Personagens;
+
+namespace AppRpgEtec.ViewModels.Personagens
+{
+    public class ListagemPersonagemViewModel : BaseViewModel
+    {
+
+        private PersonagemService pService;
+        public ObservableCollection<Personagem> Personagens { get; set; }
+
+        public ListagemPersonagemViewModel()
+        {
+            string token = Preferences.Get("UsuarioToken", string.Empty);
+            pService = new PersonagemService(token);
+            Personagens = new ObservableCollection<Personagem>();
+        }
+
+        public async Task ObterPersonagens()
+        {
+            try //Junto com o Catch -> Eitará que erros fechem o aplicativo
+            {
+                Personagens = await pService.GetPersonagensAsync();
+                OnPropertyChanged(nameof(Personagens)); // Informará a View que houve carregamento
+            }
+            catch
+            {
+                //Captará o erro para exibir em tela
+                await Application.Current.MainPage
+                    .DisplayAlert("Ops", ex.Message + " Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+    }
+}
